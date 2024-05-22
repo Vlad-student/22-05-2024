1e3713f909bfefef45076582cdf3ca315ea7f49a
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-05-22 19:02:10 +0300

    JS practice

---
+++ index.js
@@ -0,0 +1,69 @@
+console.group('1 task');
+// console.log('Напишіть програму, яка запитує у користувача довжину та ширину прямокутника, а потім обчислює та виводить площу і периметр. Підтвердіть розрахунок за допомогою confirm.');
+
+// const lengthRectangle = prompt('Введтіть довжину прямокутника');
+// const widthRectangle = prompt('Ведіть ширину прямокутника');
+
+// if (confirm('Ви хочете розрахувати площу і периметр')) {
+// const perimetrRectangle = (Number(lengthRectangle)+Number(widthRectangle))*2;
+// const areaRectangle =  (lengthRectangle*widthRectangle);
+
+//     alert('Периметр =' + perimetrRectangle + '\n Площа =' + areaRectangle)
+// }
+console.groupEnd();
+
+
+
+ console.group('2 task')
+// console.log(Math.PI);
+//  const radiusCircle = prompt('Введіть радіус кола')
+// if (confirm('Ви хочете розрахувати площу та довжину кола? ')) {
+//     const areaCircle = Math.PI * (radiusCircle*radiusCircle);
+//     const lengthCircle = Number(2)*Math.PI*Number(radiusCircle);
+//     alert('Довжина=' +lengthCircle + 'Площа=' + areaCircle );
+    
+// }
+ console.groupEnd();
+
+
+ console.group('3 task')
+// const lengthCub= prompt('Введіть довжину ребра куба')
+// if (confirm('Ви хочете розрахувати об\'єм куба?')) {
+//     const VCub = Number(lengthCub)*Number(lengthCub)*Number(lengthCub);
+//     alert('Об\'єм='+ VCub );
+// }
+ console.groupEnd;
+
+
+ console.group('4 task')
+// const radiusCilinder = prompt('Введіть радіус циліндра');
+// const heightCilinder = prompt('Введіть висоту циліндра');
+// if (confirm('Ви хочете розрахувати об\'єм куба?')) {
+//     const VCilinder = Math.PI * Number(radiusCilinder)**2 *Number(heightCilinder);
+//     alert('Result=' + VCilinder);
+// }
+ console.groupEnd();
+
+
+
+ console.group('5 task')
+// const HelloUser = ('Хочете привітання англійською');
+
+// if (confirm('англійське привітання')) {
+//     alert('Hello');
+// } else {
+//     alert('Вітаю');
+// }
+ console.groupEnd();
+
+ console.group ('6 task')
+// const платаЗаІнтернет = prompt("Скільки ви платите за інтернет щомісяця (в грн)?");
+// if ( confirm('Хочете розрахувати витрати за рік?')){
+//     let річнаСума = платаЗаІнтернет * 12;
+//     alert("Ваші щорічні витрати на інтернет становлять: " + річнаСума + " грн.");
+// } else {
+//     alert("Розрахунок річних витрат скасовано.");
+//   }
+ console.groupEnd();
+
+ 
